e780db76fcd1b3ba98002dff095a9f7162c462d7
Greg Fiumara <gregory.fiumara@nist.gov>
2021-02-22 20:01:12 -0500

    Add ReturnStatus to insight methods.

--- elft_1_x/doc/api/search/all_4.js
+++ elft_1_x/doc/api/search/all_4.js
@@ -8,7 +8,7 @@ var searchData=
   ['elft_2eh_43',['elft.h',['../elft_8h.html',1,'']]],
   ['exemplaralgorithmidentifier_44',['exemplarAlgorithmIdentifier',['../struct_e_l_f_t_1_1_extraction_interface_1_1_submission_identification.html#af4674fcf9067e24d28b6c3f0248ae452',1,'ELFT::ExtractionInterface::SubmissionIdentification']]],
   ['exists_45',['exists',['../class_e_l_f_t_1_1_search_interface.html#a58db8e5fc0f997f190de2cc77ea21ea3',1,'ELFT::SearchInterface']]],
-  ['extractcorrespondence_46',['extractCorrespondence',['../class_e_l_f_t_1_1_search_interface.html#ab62044a2f57b446e4929ac070c2b92bb',1,'ELFT::SearchInterface']]],
+  ['extractcorrespondence_46',['extractCorrespondence',['../class_e_l_f_t_1_1_search_interface.html#af1a58a446089dc78b0c4338d8b434a45',1,'ELFT::SearchInterface']]],
   ['extractioninterface_47',['ExtractionInterface',['../class_e_l_f_t_1_1_extraction_interface.html',1,'ELFT::ExtractionInterface'],['../class_e_l_f_t_1_1_extraction_interface.html#a46412b977709d82af4713260bd254762',1,'ELFT::ExtractionInterface::ExtractionInterface()']]],
-  ['extracttemplatedata_48',['extractTemplateData',['../class_e_l_f_t_1_1_extraction_interface.html#a0593d93752fb33924bbc1beaa09b934c',1,'ELFT::ExtractionInterface']]]
+  ['extracttemplatedata_48',['extractTemplateData',['../class_e_l_f_t_1_1_extraction_interface.html#a196623d6241d8d770a173bee73a04e09',1,'ELFT::ExtractionInterface']]]
 ];
