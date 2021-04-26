usnistgov/elft

commit 2cecebaf644e8a7c567210aa763fcaab68eec359

Ensure images are read correctly.
Exposes error with RGB image, which was also incorrect in data.

elft_1_x/validation/src/elft_validation.cpp
@@ -971,6 +971,20 @@ ELFT::Validation::performSingleCreate(
 			samples.emplace_back(Image(static_cast<uint8_t>(i),
 			    *md.width, *md.height, *md.ppi, *md.bpc, *md.bpp,
 			    readFile(args.imageDir / *md.filename)), md.efs);
+			const uint64_t expectedSize{
+			    static_cast<uint64_t>(*md.bpc / 8) *
+			    (*md.width) * (*md.height)};
+			if (std::get<std::optional<ELFT::Image>>(
+			    samples.back()).value().pixels.size() !=
+			    expectedSize)
+				throw std::runtime_error{"Did not read image "
+				    "correctly for imageIndex = " +
+				    ts(imageIndex) + " (expected " +
+				    std::to_string(expectedSize) + ", read " +
+				    std::to_string(std::get<
+				    std::optional<ELFT::Image>>(
+				    samples.back()).value().pixels.size()) +
+				    ')'};
 		} else
 			samples.emplace_back(std::nullopt, md.efs);
 	}

elft_1_x/validation/src/elft_validation_data.h
@@ -1306,7 +1306,7 @@ namespace ELFT::Validation::Data
 
 	    {"00002357_1C_R_L01_BP_S24_1200PPI_8BPC_3CH_LP02_1_882x1003", {
 	        {"00002357_1C_R_L01_BP_S24_1200PPI_8BPC_3CH_LP02_1_882x1003"
-	         ".gray", 882, 1003, 1200, 8, 24, EFS{0, 1200,
+	         ".gray", 882, 1003, 1200, 24, 8, EFS{0, 1200,
 	            Impression::Latent,
 	            FrictionRidgeCaptureTechnology::LatentLift,
 	            FrictionRidgeGeneralizedPosition::UnknownFinger, {},

elft_1_x/validation/validate
@@ -987,9 +987,9 @@ compile()
 	declare -A md5s
 	md5s["../libelft/libelft.cpp"]="4c12a3065a0ac853f6ca1c5c88f5acd1"
 	md5s["../include/elft.h"]="4739d4918e36a1b4e0cf069fe574c4f9"
-	md5s["src/elft_validation.cpp"]="a4ced820da9f2f54bd01b056fae10128"
+	md5s["src/elft_validation.cpp"]="24323e4b65788109bdf2a96bb6787d0f"
 	md5s["src/elft_validation.h"]="3fb8805ccb586dc33f93a3b38b24988d"
-	md5s["src/elft_validation_data.h"]="0664988a428eea09f8a8113f83ae4a2d"
+	md5s["src/elft_validation_data.h"]="26d8bf08d7cbc8cf01773fb2e91c1a39"
 	md5s["src/elft_validation_utils.h"]="c15e6eb6c1505a81721364e65a9ac46d"
 
 	# Check checksums

commit 98bd854001091a9a4b5141c1312f952d68ffab67

RGB image in validation had bpc/bpp swapped.
Not noticed because of typo introduced in
2cecebaf644e8a7c567210aa763fcaab68eec359 that was supposed to catch
issues like this.

elft_1_x/validation/src/elft_validation.cpp
@@ -972,7 +972,7 @@ ELFT::Validation::performSingleCreate(
 			    *md.width, *md.height, *md.ppi, *md.bpc, *md.bpp,
 			    readFile(args.imageDir / *md.filename)), md.efs);
 			const uint64_t expectedSize{
-			    static_cast<uint64_t>(*md.bpc / 8) *
+			    static_cast<uint64_t>(*md.bpp / 8) *
 			    (*md.width) * (*md.height)};
 			if (std::get<std::optional<ELFT::Image>>(
 			    samples.back()).value().pixels.size() !=

elft_1_x/validation/src/elft_validation_data.h
@@ -1321,7 +1321,7 @@ namespace ELFT::Validation::Data
 
 	    {"00002357_1C_R_L01_BP_S24_1200PPI_8BPC_3CH_LP02_1_882x1003", {
 	        {"00002357_1C_R_L01_BP_S24_1200PPI_8BPC_3CH_LP02_1_882x1003"
-	         ".rgb", 882, 1003, 1200, 24, 8, EFS{0, 1200,
+	         ".rgb", 882, 1003, 1200, 8, 24, EFS{0, 1200,
 	            Impression::Latent,
 	            FrictionRidgeCaptureTechnology::LatentLift,
 	            FrictionRidgeGeneralizedPosition::UnknownFinger, {},

elft_1_x/validation/validate
@@ -987,9 +987,9 @@ compile()
 	declare -A md5s
 	md5s["../libelft/libelft.cpp"]="4c12a3065a0ac853f6ca1c5c88f5acd1"
 	md5s["../include/elft.h"]="4739d4918e36a1b4e0cf069fe574c4f9"
-	md5s["src/elft_validation.cpp"]="3f302fd84bed5fb21e949aa7ae10b228"
+	md5s["src/elft_validation.cpp"]="a38b61d72fb8fa969c147905a4c87fcd"
 	md5s["src/elft_validation.h"]="3fb8805ccb586dc33f93a3b38b24988d"
-	md5s["src/elft_validation_data.h"]="4aa09059da205332c9aeed645ea87ad5"
+	md5s["src/elft_validation_data.h"]="209b78299a07b167b6c9a96a999e0531"
 	md5s["src/elft_validation_utils.h"]="c15e6eb6c1505a81721364e65a9ac46d"
 
 	# Check checksums