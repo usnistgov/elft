64cda1b7ebc4174a5d722421522e1744d1e53676
Greg Fiumara <gregory.fiumara@nist.gov>
2022-01-28 13:29:27 -0500

    Add static_assert for std::byte representation
    This should always be correct on our Intel test systems.

--- elft_1_x/include/elft.h
+++ elft_1_x/include/elft.h
@@ -1515,8 +1515,15 @@ namespace ELFT
 	/** API minor version number. */
 	uint16_t API_MINOR_VERSION{2};
 	/** API patch version number. */
-	uint16_t API_PATCH_VERSION{0};
+	uint16_t API_PATCH_VERSION{1};
 	#endif /* NIST_EXTERN_API_VERSION */
+
+	/*
+	 * Ensure that std::byte is exactly 8 bits, such that reinterpret_casts
+	 * may be safely used.
+	 */
+	static_assert(std::is_same_v<std::underlying_type_t<std::byte>,
+	    uint8_t>, "std::byte not represented as unsigned 8 bit type");
 }
 
 #endif /* ELFT_H_ */
