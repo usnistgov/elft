usnistgov/elft

commit c9f447e9b859dfda4455cd15547c6be3ffb2c1b5

Fix endian typo, provide more examples.

elft_1_x/include/elft.h
@@ -236,14 +236,15 @@ namespace ELFT
 		 * @param bpc
 		 * Number of bits used by each color component (8 or 16).
 		 * @param bpp
-		 * Number of bits comprising a single pixel.
+		 * Number of bits comprising a single pixel (8, 16, 24, or 48).
 		 * @param pixels
 		 * #width * #height * (#bpp / #bpc) bytes of image data, with
 		 * `pixels.front()` representing the first byte of the top-left
 		 * pixel, and `pixels.back()` representing the last byte of
-		 * bottom-right pixel. It is decompressed little endian image
+		 * bottom-right pixel. It is decompressed big endian image
 		 * data, canonically coded as defined in ISO/IEC 19794-4:2005,
-		 * section 6.2.
+		 * section 6.2. For example, 0xFF00 is closer to white than it
+		 * is to black.
 		 *
 		 * @note
 		 * Number of color components is #bpp / #bpc and shall be either
@@ -271,7 +272,10 @@ namespace ELFT
 		uint16_t ppi{};
 		/** Number of bits used by each color component (8 or 16). */
 		uint8_t bpc{};
-		/** Number of bits comprising a single pixel. */
+		/**
+		 * Number of bits comprising a single pixel (8, 16, 24, or
+		 * 48).
+		 */
 		uint8_t bpp{};
 		/**
 		 * @brief
@@ -523,7 +527,9 @@ namespace ELFT
 		 * @details
 		 * Coordinate are relative to the bounding rectangle created by
 		 * EFS::roi, if supplied. Otherwise, they are relative to the
-		 * the source image.
+		 * the source image. Add the minimum X and Y values from #roi
+		 * to convert ROI-relative Coordinate to image-relative
+		 * Coordinate.
 		 */
 		std::vector<Coordinate> region{};
 		/** Clarity of ridge features enclosed within #region. */

elft_1_x/validation/validate
@@ -986,7 +986,7 @@ compile()
 
 	declare -A md5s
 	md5s["../libelft/libelft.cpp"]="f804aba2bed873154a8985d3909e889b"
-	md5s["../include/elft.h"]="bcac81664473eccb724527d9e3c03c0c"
+	md5s["../include/elft.h"]="aac5c43154c79b027d9a52bc469f161d"
 	md5s["src/elft_validation.cpp"]="064f6e7cc54adb1d5df56cf68dc766c3"
 	md5s["src/elft_validation.h"]="3fb8805ccb586dc33f93a3b38b24988d"
 	md5s["src/elft_validation_data.h"]="b3ccc363cd4da00c65a2fec21925f873"

commit 9738973588c9647fedf8bd6134b69abc8a01f685

Fix endian typo in documentation.
Regression since c9f447e.

elft_1_x/include/elft.h
@@ -285,8 +285,10 @@ namespace ELFT
 		 * #width * #height * (#bpp / #bpc) bytes of image data, with
 		 * `pixels.front()` representing the first byte of the top-left
 		 * pixel, and `pixels.back()` representing the last byte of
-		 * bottom-right pixel. It is decompressed little endian image
+		 * bottom-right pixel. It is decompressed big endian image
 		 * data, canonically coded as defined in ISO/IEC 19794-4:2005,
+		 * section 6.2. For example, 0xFF00 is closer to white than it
+		 * is to black.
 		 *
 		 * @note
 		 * To pass pixels to a C-style array, invoke pixel's `data()`

elft_1_x/validation/validate
@@ -1095,7 +1095,7 @@ compile()
 
 	declare -A md5s
 	md5s["../libelft/libelft.cpp"]="b7105274543b422e51c656d8db36b165"
-	md5s["../include/elft.h"]="95289613b084808449eb4da6a13f7913"
+	md5s["../include/elft.h"]="3c391201ec6d8f1ea3581bd82c35b450"
 	md5s["src/elft_validation.cpp"]="ecf20e7e7d2c6ae4236cd2f277062efb"
 	md5s["src/elft_validation.h"]="c27629ef53a2f86874fe7b89de301d49"
 	md5s["src/elft_validation_data.h"]="e2d931dc343a149f0f6ab409b83f611f"