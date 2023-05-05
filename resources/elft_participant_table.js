b28803af04c4f51347ed7375782b192cecec1b81
Greg Fiumara <gregory.fiumara@nist.gov>
2023-05-05 07:24:27 -0400

    Fix indentation + add/use minified version

--- resources/elft_participant_table.js
+++ resources/elft_participant_table.js
@@ -62,18 +62,18 @@ $(document).ready(function() {
 		fixedColumns: {
 			left: 1,
 		},
-        responsive: {
-          details: {
-            display: $.fn.dataTable.Responsive.display.modal({
-              header: function (row) {
-                var data = row.data();
-                return 'Details for ' + data[0];
-              }
-            }),
-            renderer: $.fn.dataTable.Responsive.renderer.tableAll({
-              tableClass: 'ui table'
-            })
-          }
-        }
+		responsive: {
+			details: {
+				display: $.fn.dataTable.Responsive.display.modal({
+					header: function (row) {
+						var data = row.data();
+						return 'Details for ' + data[0];
+					}
+				}),
+				renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+					tableClass: 'ui table'
+				})
+			}
+		}
 	});
 });
